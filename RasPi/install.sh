78438188100879fc7b70d41f0941aa0fb09d8159
Jordan O'Briant <jordanob.30@gmail.com>
2025-03-15 13:11:31 -0400

    Add systemd.service activation to install script

--- RasPi/install.sh
+++ RasPi/install.sh
@@ -2,5 +2,17 @@
 
 # Install the required packages
 sudo apt update
-sudo apt install python3
-sudo apt install python3-lgpio
+sudo apt install -y python3 python3-lgpio
+
+# Move UART.service to the systemd directory
+sudo cp /home/jordan/Documents/Projects/School/EE481/Wasted_Energy/RasPi/UART.service /etc/systemd/system/
+
+# Reload systemd daemon to recognize the new service
+sudo systemctl daemon-reload
+
+# Enable the UART service to start on boot
+sudo systemctl enable UART.service
+
+# Start the UART service immediately
+sudo systemctl start UART.service
+
